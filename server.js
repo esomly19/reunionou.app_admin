55868367298d45347400537bf2fdaf589b47a388
Thomas <esomly57@gmail.com>
2020-06-24 10:59:06 +0200

    correction

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var path = require('path')
+const cors = require('cors')
 var serveStatic = require('serve-static')
 
 var app = express()
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+app.use(bodyParser.json());
+app.use(cors())
 app.use(serveStatic(path.join(__dirname, 'dist')))
 var port = process.env.PORT || 5000
 
