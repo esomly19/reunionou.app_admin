cc5ee3cef7ed00e1288b3bbd5186d69886fbda00
Thomas <esomly57@gmail.com>
2020-06-24 17:09:15 +0200

    correction

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -21,18 +21,26 @@ export default {
         this.gradient2 = this.$refs.canvas
           .getContext("2d")
           .createLinearGradient(0, 0, 0, 450);
+        this.gradient3 = this.$refs.canvas
+          .getContext("2d")
+          .createLinearGradient(0, 0, 0, 450);
 
         this.gradient.addColorStop(0, "rgba(255, 0,0 )");
 
         this.gradient2.addColorStop(0, "rgba(0, 231, 255)");
+        this.gradient3.addColorStop(0, "rgba(0, 255, 0)");
 
         this.renderChart(
           {
-            labels: ["Chrome", "Firefox"],
+            labels: ["Chrome", "Firefox", "Autre"],
             datasets: [
               {
-                backgroundColor: [this.gradient, this.gradient2],
-                data: [res.data.chrome, res.data.firefox]
+                backgroundColor: [
+                  this.gradient,
+                  this.gradient2,
+                  this.gradient3
+                ],
+                data: [res.data.chrome, res.data.firefox, res.data.autre]
               }
             ]
           },
